8365d86a8bf443bd6f4e004dbbd7289c1fd1490e
Butter <93381536+Mishaix@users.noreply.github.com>
2021-12-03 11:49:36 +0000

    how about this?

--- main.py
+++ main.py
@@ -111,10 +111,8 @@ def man(message):
 @bot.message_handler(commands=['obd'])
 def opsbluedagger(message):
     markup = types.InlineKeyboardMarkup()
-    str1= markup.row(types.InlineKeyboardButton(text=str(today),callback_data="1"))
-    str2= markup.row(types.InlineKeyboardButton(text=str(tomorrow),callback_data="2"))
-    markup.add(str1,str2)
-    return markup
+    markup.add(str1= markup.row(types.InlineKeyboardButton(text=str(today),callback_data="1")))
+    markup.add(str2= markup.row(types.InlineKeyboardButton(text=str(tomorrow),callback_data="2")))
     msg = bot.reply_to(message, "What is the date (DDMMYY)?", reply_markup=markup)
     bot.register_next_step_handler(msg, process_date_step)
 
