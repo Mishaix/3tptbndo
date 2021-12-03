83bd29a28d6bde2e9ea353d20f7d536cf6535007
Butter <93381536+Mishaix@users.noreply.github.com>
2021-12-03 11:41:35 +0000

    working??

--- main.py
+++ main.py
@@ -111,8 +111,9 @@ def man(message):
 @bot.message_handler(commands=['obd'])
 def opsbluedagger(message):
     markup = types.InlineKeyboardMarkup()
-    str1= markup.row(types.InlineKeyboardButton(text=str(today),callback_data="1"))
-    str2= markup.row(types.InlineKeyboardButton(text=str(tomorrow),callback_data="2"))
+    str1= types.InlineKeyboardButton(text=str(today),callback_data="1")
+    str2= types.InlineKeyboardButton(text=str(tomorrow),callback_data="2")
+    markup.add(str1, str2)
     msg = bot.reply_to(message, "What is the date (DDMMYY)?", reply_markup=markup)
     bot.register_next_step_handler(msg, process_date_step)
 
